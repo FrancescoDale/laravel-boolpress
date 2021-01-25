560708ded1ebb050752b52eca6ba4ddb71d551de
FrancescoDale <fd060862@gmail.com>
2021-01-25 16:01:23 +0100

    creazione colonne tabella posts

--- database/migrations/2021_01_25_144946_create_posts_table.php
+++ database/migrations/2021_01_25_144946_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->string('summary', 100)->nullable();
+            $table->text('body');
             $table->timestamps();
         });
     }
