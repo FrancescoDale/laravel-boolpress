4fad2ac128c584d04997d9e1d55e6660074155b9
FrancescoDale <fd060862@gmail.com>
2021-01-27 18:33:11 +0100

    creata una view in cui vengono stampati gli elementi della tabella posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                        <tr class="text-capitalize">
+                            <td>id</td>
+                            <td>title</td>
+                            <td>slug</td>
+                            <td>body</td>
+                            <td>azioni</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td>{{ $post->title}}</td>
+                                <td>{{ $post->slug}}</td>
+                                <td>{{ $post->body}}</td>
+                                <td>
+                                    <a class="btn btn-info text-capitalize" href="#">dettagli</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
