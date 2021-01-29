16a6f21b8553cd1624907b027cc357cc4956120f
FrancescoDale <fd060862@gmail.com>
2021-01-29 20:32:00 +0100

    fix nomi e aggiunta la possibilità di selezionare le categorie dei posts

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
     public function create()
     {
         $data = [
-            'category' => Category::all();
+            'categories' => Category::all()
         ];
         return view('admin.posts.create', $data);
     }
