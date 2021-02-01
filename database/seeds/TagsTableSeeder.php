19d16b5ab74c7a02412fd0f9ef2732357245c09e
FrancescoDale <fd060862@gmail.com>
2021-02-01 21:05:57 +0100

    corpo del seeder e popolamento tabella

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -11,11 +11,31 @@ class TagsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         for ($i=0; $i < 5; $i++) {
 
-            
+            $new_tag = new Tag();
+            $new_tag->name = $faker->words(3, true);
+
+            // genero lo slug
+            $slug = Str::slug($new_tag->name);
+            $slug_base = $slug;
+            // verifico che lo slug non esista nel database
+            $tag_presente = Tag::where('slug', $slug)->first();
+            $cont = 1;
+            // entro nel ciclo while se ho trovato un post con lo stesso $slug
+            while($tag_presente) {
+                // genero un nuovo slug aggiungendo il contatore alla fine
+                $slug = $slug_base . '-' . $cont;
+                $cont++;
+                $tag_presente = Tag::where('slug', $slug)->first();
+            }
+            // quando esco dal while sono sicuro che lo slug non esiste nel db
+            // assegno lo slug al post
+            $new_tag->slug = $slug;
+            $new_tag->save();
+
         }
 
     }
